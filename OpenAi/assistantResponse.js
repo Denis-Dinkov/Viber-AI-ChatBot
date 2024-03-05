7719b3a2191785ffb675665d901a7d31a0a664c7
Denis Dinkov <denisdinkov7@gmail.com>
2024-03-05 21:40:39 +0200

    Update assistantResponse.js

--- OpenAi/assistantResponse.js
+++ OpenAi/assistantResponse.js
@@ -2,42 +2,30 @@ const TextMessage = require("viber-bot").Message.Text;
 const OpenAI = require("openai");
 const openai = new OpenAI();
 
-let chatHistory = []; // Move chatHistory outside the function
+let messages = [];
 
 async function assistantResponse(botResponse, text_received, say) {
-  let isResponseReceived = false;
+  messages.push({ role: "user", content: text_received });
 
   const timeoutId = setTimeout(() => {
-    if (!isResponseReceived) {
-      say(botResponse, "One second...Let me check!");
-    }
-  }, 1000); // 1000 milliseconds = 1 second
+    say(botResponse, "One second...Let me check!");
+  }, 1000);
 
   try {
-    const messages = chatHistory.map(({ role, content }) => {
-      return { role, content }; // Return the object
-    });
-
-    messages.push({ role: "user", content: text_received });
-
     let response = await openai.chat.completions.create({
       messages,
       model: "gpt-3.5-turbo",
     });
 
-    isResponseReceived = true;
-    clearTimeout(timeoutId);
-
     let message = response.choices[0].message.content;
     botResponse.send(new TextMessage(message));
 
-    chatHistory.push({ role: "user", content: text_received });
-    chatHistory.push({ role: "assistant", content: message });
+    messages.push({ role: "assistant", content: message });
   } catch (error) {
-    isResponseReceived = true;
-    clearTimeout(timeoutId); // clear the timeout if an error occurs
     console.error("An error occurred:", error);
     say(botResponse, "Sorry, I couldn't process your request.");
+  } finally {
+    clearTimeout(timeoutId); // clear the timeout always
   }
 }
 
