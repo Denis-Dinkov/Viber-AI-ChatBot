b635c978bffdd8a7f9f965fb9d68ca060801eca2
Denis Dinkov <denisdinkov7@gmail.com>
2024-03-05 20:38:41 +0200

    installed openai api

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 "use strict";
 
 require("dotenv").config();
+const OpenAI = require("openai");
+const openai = new OpenAI();
 const ViberBot = require("viber-bot").Bot;
 const BotEvents = require("viber-bot").Events;
-
 const TextMessage = require("viber-bot").Message.Text;
-const UrlMessage = require("viber-bot").Message.Url;
-const ContactMessage = require("viber-bot").Message.Contact;
 const PictureMessage = require("viber-bot").Message.Picture;
-const VideoMessage = require("viber-bot").Message.Video;
-const LocationMessage = require("viber-bot").Message.Location;
-const StickerMessage = require("viber-bot").Message.Sticker;
-const FileMessage = require("viber-bot").Message.File;
-const RichMediaMessage = require("viber-bot").Message.RichMedia;
-const KeyboardMessage = require("viber-bot").Message.Keyboard;
 
 const ngrok = require("./get_public_url");
 
@@ -21,67 +14,41 @@ function say(response, message) {
   response.send(new TextMessage(message));
 }
 
-function checkUrlAvailability(botResponse, text_received) {
+async function checkUrlAvailability(botResponse, text_received) {
   let sender_name = botResponse;
   let sender_id = botResponse.userProfile.id;
-  console.log(botResponse);
 
-  say(botResponse, "One second...Let me check!");
+  let isResponseReceived = false;
 
-  let message;
-  if (text_received === "text") {
-    // ================================
-    // TextMessage object
-    // ================================
-    message = new TextMessage("hello world");
-  } else if (text_received === "contact") {
-    // ================================
-    // Contact Message object
-    // ================================
-    let contactName = "Ko Ko";
-    let contactPhoneNumber = "09420084765";
-    message = new ContactMessage(contactName, contactPhoneNumber);
-  } else if (text_received === "rich_media") {
-    // ================================
-    // RichMedia Message object
-    // ================================
-    const SAMPLE_RICH_MEDIA = {
-      ButtonsGroupColumns: 6,
-      ButtonsGroupRows: 5,
-      BgColor: "#FFFFFF",
-      Buttons: [
-        {
-          ActionBody: "https://www.google.com",
-          ActionType: "open-url",
-          BgMediaType: "picture",
-          Image:
-            "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
-          BgColor: "#000000",
-          TextOpacity: 60,
-          Rows: 4,
-          Columns: 6,
-        },
-        {
-          ActionBody: "https://www.facebook.com",
-          ActionType: "open-url",
-          BgColor: "#85bb65",
-          Text: "Buy",
-          TextOpacity: 60,
-          Rows: 1,
-          Columns: 6,
-        },
-      ],
-    };
-    message = new RichMediaMessage(SAMPLE_RICH_MEDIA);
+  const timeoutId = setTimeout(() => {
+    if (!isResponseReceived) {
+      say(botResponse, "One second...Let me check!");
+    }
+  }, 1000); // 1000 milliseconds = 1 second
+
+  try {
+    let response = await openai.chat.completions.create({
+      messages: [{ role: "system", content: text_received }],
+      model: "gpt-3.5-turbo",
+    });
+
+    isResponseReceived = true;
+    clearTimeout(timeoutId); // clear the timeout if the response is received
+
+    let message = response.choices[0].message.content;
+    botResponse.send(new TextMessage(message));
+  } catch (error) {
+    isResponseReceived = true;
+    clearTimeout(timeoutId); // clear the timeout if an error occurs
+    console.error("An error occurred:", error);
+    say(botResponse, "Sorry, I couldn't process your request.");
   }
-
-  botResponse.send(message);
 }
 
 const bot = new ViberBot({
   authToken: process.env.ACCESS_TOKEN,
   name: "Dendo",
-  avatar: "https://developers.viber.com/docs/img/stickers/40122.png", // It is recommended to be 720x720, and no more than 100kb.
+  avatar: "./profileimg.jpg", // It is recommended to be 720x720, and no more than 100kb.
 });
 
 // The user will get those messages on first registration
