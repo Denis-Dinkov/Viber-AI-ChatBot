f3a0ab9a73f6c916ad9fc176797b371f4c43efa0
Denis Dinkov <denisdinkov7@gmail.com>
2024-03-12 18:33:41 +0200

    ,

---
+++ client/src/components/ProtectedLayout/header.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
+
+import { Layout, Button, theme } from "antd";
+const { Header } = Layout;
+
+import { CurrentUser } from "../currentUser";
+const HeaderComponent = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  return (
+    <Header
+      style={{
+        background: colorBgContainer,
+        display: "flex",
+        justifyContent: "flex-end",
+        alignItems: "center",
+        padding: "0px 24px",
+        height: "64px",
+        position: "sticky",
+        top: 0,
+        zIndex: 999,
+      }}
+    >
+      <CurrentUser />
+    </Header>
+  );
+};
+
+export default HeaderComponent;
