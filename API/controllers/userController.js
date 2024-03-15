db00fbc300f0fb4e0d7759bc068ff30ade478ccb
Daniel Valchev <dvalchevvv@gmail.com>
2024-03-15 11:08:07 +0200

    Update userController.js

--- API/controllers/userController.js
+++ API/controllers/userController.js
@@ -1,7 +1,7 @@
 const userService = require('../services/userService');
 
 const getUsers = async (req, res) => {
-  const { id }= req.params;
+  const { id } = req.query;
   try {
     if(!id) throw new Error('No user id provided');
     const users = await userService.getUsers(id);
